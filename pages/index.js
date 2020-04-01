4a42d304559ec97e4a70c6d1ceb96c9882f1960e
s-fletcher <sfletchertx@gmail.com>
2020-04-01 02:30:02 -0500

    Minimum styling and basic implementation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useQuery, gql } from "@apollo/client";
 import Head from "next/head";
-import styled from 'styled-components'
+import styled from "styled-components";
 import NavBar from "../components/NavBar";
 import Category from "../components/Dashboard/Category";
 
@@ -21,7 +21,19 @@ const CATEGORIES = gql`
     }
 `;
 
-const StyledIndex = styled.div``
+const StyledIndex = styled.div`
+    column-count: 2; /* Maximum number of columns */
+    column-width: 230px; /* TWEAK Minimum width for each column */
+    column-gap: 0;
+    max-width: 650px; /* TWEAK */
+    margin: 50px auto;
+    padding: 0 50px;
+    text-align: left;
+    position: relative;
+    @media screen and (max-width: ${props => props.theme.mobileWidth}) {
+        padding: 0 20px;
+    }
+`;
 
 function Index() {
     const { loading, error, data } = useQuery(CATEGORIES);
@@ -32,23 +44,25 @@ function Index() {
     if (error) return <p>{error.message}</p>;
     /** RETURN Dashboard */
     return (
-        <StyledIndex>
+        <div>
             <Head>
                 <title>Wiki</title>
             </Head>
             <NavBar />
-            {Object.keys(data.categories).map(i => {
-                return (
-                    <Category
-                        key={data.categories[i].name}
-                        name={data.categories[i].name}
-                        index={data.categories[i].index}
-                        emoji={data.categories[i].emoji}
-                        pages={data.categories[i].pages}
-                    />
-                );
-            })}
-        </StyledIndex>
+            <StyledIndex>
+                {Object.keys(data.categories).map(i => {
+                    return (
+                        <Category
+                            key={data.categories[i].name}
+                            name={data.categories[i].name}
+                            index={data.categories[i].index}
+                            emoji={data.categories[i].emoji}
+                            pages={data.categories[i].pages}
+                        />
+                    );
+                })}
+            </StyledIndex>
+        </div>
     );
 }
 
