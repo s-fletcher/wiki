d957cc3a6ef7b9224b99c0dc79f9578b0acc2ebf
s-fletcher <sfletchertx@gmail.com>
2020-04-04 17:35:25 -0500

    Changed how themes are called

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,13 @@ import { ThemeProvider } from "styled-components";
 import "../public/defaults.css";
 import "../public/nprogress.css";
 import client from "../lib/client";
-import { lightTheme, darkTheme } from "../lib/themes";
 import Head from "next/head";
+import { themes } from "../lib/themes.js";
 
 function MyApp({ Component, pageProps }) {
     return (
         <ApolloProvider client={client}>
-            <ThemeProvider theme={lightTheme}>
+            <ThemeProvider theme={themes.light}>
                 <Head>
                     <title>Wiki</title>
                 </Head>
