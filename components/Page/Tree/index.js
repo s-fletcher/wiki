4903afe2445a127fc130e57084e54065db36b800
s-fletcher <sfletchertx@gmail.com>
2020-04-12 00:56:11 -0500

    Basic tree implementation, no styling

--- components/Page/Tree/index.js
+++ components/Page/Tree/index.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import TreeItem from "./TreeItem";
+import { gql, useQuery } from "@apollo/client";
 
 const StyledTree = styled.div`
     min-width: 230px;
@@ -8,8 +9,42 @@ const StyledTree = styled.div`
     max-width: max-content;
 `;
 
-function Tree() {
-    return <StyledTree><TreeItem category={"Sponsoring"} /></StyledTree>;
+const CATEGORIES = gql`
+    {
+        categories(orderBy: index_ASC) {
+            name
+            emoji
+            pages(orderBy: index_ASC) {
+                name
+                serializedName
+            }
+        }
+    }
+`;
+
+function Tree({currentPage}) {
+    const { loading, error, data } = useQuery(CATEGORIES);
+
+    /** RETURN Loading */
+    if (loading) return <p>Loading...</p>;
+    /** RETURN Error */
+    if (error) return <p>{error.message}</p>;
+    /** RETURN Dashboard */
+    return (
+        <StyledTree>
+            {Object.keys(data.categories).map((i) => {
+                return (
+                    <TreeItem
+                        key={data.categories[i].name}
+                        name={data.categories[i].name}
+                        emoji={data.categories[i].emoji}
+                        pages={data.categories[i].pages}
+                        currentPage={currentPage}
+                    />
+                );
+            })}
+        </StyledTree>
+    );
 }
 
 export default Tree;
