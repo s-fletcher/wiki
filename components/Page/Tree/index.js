6456bb49db2b9e4ed243f5b425e8bf7b2b411ddc
s-fletcher <sfletchertx@gmail.com>
2020-04-12 19:10:20 -0500

    Better fixed scrolling

--- components/Page/Tree/index.js
+++ components/Page/Tree/index.js
@@ -6,10 +6,20 @@ import styled from "styled-components";
 import TreeItem from "./TreeItem";
 
 const StyledTree = styled.div`
+    /* min-width: 230px; */
+    /* width: 32%; */
     min-width: 230px;
-    background: lightgray;
-    width: 32%;
     max-width: max-content;
+    position: relative;
+    .items {
+        position: absolute;
+        width: 100%;
+    }
+    .sticky {
+        top: 25px;
+        width: 230px;
+        position: fixed;
+    }
 `;
 
 function Tree({ currentPage, loading, error, data }) {
@@ -18,19 +28,39 @@ function Tree({ currentPage, loading, error, data }) {
     /** RETURN Error */
     if (error) return <p>{error.message}</p>;
     /** RETURN Dashboard */
+
+    React.useEffect(() => {
+        window.onscroll = function () {
+            myFunction();
+        };
+
+        var header = document.getElementById("stickyTree");
+        var sticky = header.getBoundingClientRect().top - 25;
+
+        function myFunction() {
+            if (window.pageYOffset > sticky) {
+                header.classList.add("sticky");
+            } else {
+                header.classList.remove("sticky");
+            }
+        }
+    }, []);
+
     return (
         <StyledTree>
-            {Object.keys(data.categories).map((i) => {
-                return (
-                    <TreeItem
-                        key={data.categories[i].name}
-                        name={data.categories[i].name}
-                        emoji={data.categories[i].emoji}
-                        pages={data.categories[i].pages}
-                        currentPage={currentPage}
-                    />
-                );
-            })}
+            <div className="items" id="stickyTree">
+                {Object.keys(data.categories).map((i) => {
+                    return (
+                        <TreeItem
+                            key={data.categories[i].name}
+                            name={data.categories[i].name}
+                            emoji={data.categories[i].emoji}
+                            pages={data.categories[i].pages}
+                            currentPage={currentPage}
+                        />
+                    );
+                })}
+            </div>
         </StyledTree>
     );
 }
