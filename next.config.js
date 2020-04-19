65e85a861684ef5ef82bcd1b0f375b44443a651f
s-fletcher <sfletchertx@gmail.com>
2020-04-19 15:57:22 -0500

    Fixed build env config

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    env: {
+        SLACK_CLIENT_ID: process.env.SLACK_CLIENT_ID,
+        SLACK_CLIENT_SECRET: process.env.SLACK_CLIENT_SECRET,
+        TEAM_ID: process.env.TEAM_ID,
+    },
+};
