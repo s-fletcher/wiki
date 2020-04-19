04b0918e0249bf3454315071e7b13f5572c1ab64
s-fletcher <sfletchertx@gmail.com>
2020-04-19 16:05:45 -0500

    Added prisma endpoint as env

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ module.exports = {
         SLACK_CLIENT_ID: process.env.SLACK_CLIENT_ID,
         SLACK_CLIENT_SECRET: process.env.SLACK_CLIENT_SECRET,
         TEAM_ID: process.env.TEAM_ID,
+        PRISMA_ENDPOINT: process.env.PRISMA_ENDPOINT,
     },
 };
